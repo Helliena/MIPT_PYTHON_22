f48d1bd1154e97a589c385f68dc1f6dd236708f3
Lena Leoverenskaya <dudko.ev@phystech.edu>
2022-05-21 02:43:53 +0300

    Removed constants, includes

--- Shadow_client_server/cat.py
+++ Shadow_client_server/cat.py
@@ -1,25 +1,25 @@
 import time
-from collections import defaultdict
-
-from numpy import character
-
 
 class Cat:
     def __init__(self):
         self.satiety = 100
         self.satiety_last_update = time.time()
+        self.satiety_update_speed = 0.5
         self.drunk = 100
         self.drunk_last_update = time.time()
+        self.drunk_update_speed = 0.3
         self.chill = 100
         self.chill_last_update = time.time()
+        self.chill_update_speed = 0.7
         self.sport = 100
         self.sport_last_update = time.time()
+        self.sport_update_speed = 0.1
 
     def update(self):
-        self.satiety = round(100 - (time.time() - self.satiety_last_update) * 0.5, 2)
-        self.drunk = round(100 - (time.time() - self.drunk_last_update) * 0.3, 2)
-        self.chill = round(100 - (time.time() - self.chill_last_update) * 0.7, 2)
-        self.sport = round(100 - (time.time() - self.sport_last_update) * 0.1, 2)
+        self.satiety = round(100 - (time.time() - self.satiety_last_update) * self.satiety_update_speed, 2)
+        self.drunk = round(100 - (time.time() - self.drunk_last_update) * self.drunk_update_speed, 2)
+        self.chill = round(100 - (time.time() - self.chill_last_update) * self.chill_update_speed, 2)
+        self.sport = round(100 - (time.time() - self.sport_last_update) * self.sport_update_speed, 2)
 
     def eat(self):
         self.satiety_last_update = time.time()
